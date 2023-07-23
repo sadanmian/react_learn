da06338afc87c3f361ea21d4a37feb68ab875f6f
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-23 22:48:05 +0530

    Methods as props

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -7,11 +7,13 @@ import Counter from "./components/Counter";
 import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
+import ParentComponents from "./components/ParentComponents";
 
 function App() {
   return (
     <div className="App">
-      <EventBind />
+      <ParentComponents />
+      {/* <EventBind /> */}
       {/* <FunctionClick /> */}
       {/* <ClassClick /> */}
       {/* <Counter /> */}
