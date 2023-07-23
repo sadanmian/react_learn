f79101bf84154713af9ac6667389327785717791
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-23 11:18:24 +0530

    greet

---
+++ hello-world/src/components/Greet.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function Greet() {
+//     return(
+//         <div>Hello Sadan Mian</div>
+//     )
+// }
+
+export const Greet = () => <h1>Hello Sadan</h1>;
+
+// export default Greet;
