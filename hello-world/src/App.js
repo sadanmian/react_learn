cf06c020568dc5cf8764c2fec17e28ed11ca48e6
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-23 23:10:28 +0530

    Conditional Rendering

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -8,11 +8,13 @@ import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
 import ParentComponents from "./components/ParentComponents";
+import UserGreeting from "./components/UserGreeting";
 
 function App() {
   return (
     <div className="App">
-      <ParentComponents />
+      <UserGreeting />
+      {/* <ParentComponents /> */}
       {/* <EventBind /> */}
       {/* <FunctionClick /> */}
       {/* <ClassClick /> */}
