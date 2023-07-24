605ee9ad9015684a7d7b7164e002543d46acd08f
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-24 15:33:47 +0530

    Refs with Class Components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -22,11 +22,13 @@ import Table from "./components/Table";
 import PureComp from "./components/PureComp";
 import ParentComp from "./components/ParentComp";
 import RefsDemo from "./components/RefsDemo";
+import FocusInput from "./components/FocusInput";
 
 function App() {
   return (
     <div className="App">
-      <RefsDemo />
+      <FocusInput />
+      {/* <RefsDemo /> */}
       {/* <ParentComp /> */}
       {/* <PureComp /> */}
       {/* <Table /> */}
