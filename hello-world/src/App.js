0adced9ef686ba32a9c07e30dbfcaf59273ad05b
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-24 19:47:16 +0530

    Portals

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -24,11 +24,13 @@ import ParentComp from "./components/ParentComp";
 import RefsDemo from "./components/RefsDemo";
 import FocusInput from "./components/FocusInput";
 import FRParentInput from "./components/FRParentInput";
+import PortalDemo from "./components/PortalDemo";
 
 function App() {
   return (
     <div className="App">
-      <FRParentInput />
+      <PortalDemo />
+      {/* <FRParentInput /> */}
       {/* <FocusInput /> */}
       {/* <RefsDemo /> */}
       {/* <ParentComp /> */}
