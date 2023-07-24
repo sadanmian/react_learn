9215442ba110f869c72b36fd382b7c5ec772798a
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-24 20:05:02 +0530

    Error Boundary

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -25,11 +25,22 @@ import RefsDemo from "./components/RefsDemo";
 import FocusInput from "./components/FocusInput";
 import FRParentInput from "./components/FRParentInput";
 import PortalDemo from "./components/PortalDemo";
+import Hero from "./components/Hero";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
     <div className="App">
-      <PortalDemo />
+      <ErrorBoundary>
+        <Hero heroName={"Batman"} />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName={"Superman"} />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName={"Joker"} />
+      </ErrorBoundary>
+      {/* <PortalDemo /> */}
       {/* <FRParentInput /> */}
       {/* <FocusInput /> */}
       {/* <RefsDemo /> */}
