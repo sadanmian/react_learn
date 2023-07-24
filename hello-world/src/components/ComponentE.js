ab3996e93efcfe27a70df9a33ea85ca27ee74009
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-24 23:29:40 +0530

    Context (Part 3)

--- hello-world/src/components/ComponentE.js
+++ hello-world/src/components/ComponentE.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import ComponentF from "./ComponentF";
+import UserContext from "./userContext";
 
 class ComponentE extends Component {
+  static contextType = UserContext;
   render() {
     return (
       <div>
+        Component E context {this.context}
         <ComponentF />
       </div>
     );
