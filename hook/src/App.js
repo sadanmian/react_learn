f39e985e4f4a5b42a5c09105db5fd8b5828c49c8
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-25 11:12:53 +0530

    useState with object

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
+import HookCounterThree from "./components/HookCounterThree";
 import HookCounterTwo from "./components/HookCounterTwo";
 
 function App() {
   return (
     <div className="App">
-      <HookCounterTwo />
+      <HookCounterThree />
+      {/* <HookCounterTwo /> */}
       {/* <ClassCounter /> */}
       {/* <HookCounter /> */}
     </div>
