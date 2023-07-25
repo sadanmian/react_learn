9082c640639b2625c36b625e1cd6117fb2574983
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-25 14:17:07 +0530

    Run effects only once

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import ClassCounterOne from "./components/ClassCounterOne";
+import ClassMouse from "./components/ClassMouse";
 import HookCounter from "./components/HookCounter";
 import HookCounterFour from "./components/HookCounterFour";
 import HookCounterOne from "./components/HookCounterOne";
 import HookCounterThree from "./components/HookCounterThree";
 import HookCounterTwo from "./components/HookCounterTwo";
+import HookMouse from "./components/HookMouse";
 
 function App() {
   return (
     <div className="App">
-      <HookCounterOne />
+      <HookMouse />
+      {/* <ClassMouse /> */}
+      {/* <HookCounterOne /> */}
       {/* <ClassCounterOne /> */}
       {/* <HookCounterFour /> */}
       {/* <HookCounterThree /> */}
