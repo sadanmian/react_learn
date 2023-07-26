922f554457b69e59b698d7c75128e1e5af9b2a8c
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-26 15:11:58 +0530

    useInput Custom Hook

--- callback-hook/src/App.js
+++ callback-hook/src/App.js
@@ -9,12 +9,14 @@ import DocTitleOne from "./components/DocTitleOne";
 import DocTitleTwo from "./components/DocTitleTwo";
 import CounterOne from "./components/CounterOne";
 import CounterTwo from "./components/CounterTwo";
+import UserForm from "./components/UserForm";
 
 function App() {
   return (
     <div className="App">
-      <CounterOne />
-      <CounterTwo />
+      <UserForm />
+      {/* <CounterOne /> */}
+      {/* <CounterTwo /> */}
       {/* <DocTitleOne /> */}
       {/* <DocTitleTwo /> */}
       {/* <HookTimer /> */}
