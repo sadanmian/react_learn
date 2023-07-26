351ff9a7c615cffba496afd431986406cea1cdad
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-26 21:35:40 +0530

    Parent and Child

--- react-render/src/App.js
+++ react-render/src/App.js
@@ -4,12 +4,14 @@ import { UseState } from "./components/UseState/UseState";
 import { UseReducer } from "./components/UseReducer/UseReducer";
 import { ObjectUseState } from "./components/Immutable State/ObjectUseState";
 import { ArrayUseState } from "./components/Immutable State/ArrayUseState";
+import { Parent } from "./components/Parent Child/Parent";
 
 function App() {
   return (
     <div className="App">
-      <ObjectUseState />
-      <ArrayUseState />
+      <Parent />
+      {/* <ObjectUseState /> */}
+      {/* <ArrayUseState /> */}
       {/* <UseReducer /> */}
       {/* <UseState /> */}
     </div>
