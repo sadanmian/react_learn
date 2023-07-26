92a1d5fb92bd11357db3603fb0c4a387c3adb68d
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-26 11:42:49 +0530

    useRef Hook Part 2

--- callback-hook/src/App.js
+++ callback-hook/src/App.js
@@ -3,11 +3,15 @@ import React from "react";
 import ParentComponent from "./components/ParentComponent";
 import Counter from "./components/Counter";
 import FocusInput from "./components/FocusInput";
+import ClassTimer from "./components/ClassTimer";
+import HookTimer from "./components/HookTimer";
 
 function App() {
   return (
     <div className="App">
-      <FocusInput />
+      <HookTimer />
+      <ClassTimer />
+      {/* <FocusInput /> */}
       {/* <Counter /> */}
       {/* <ParentComponent /> */}
     </div>
