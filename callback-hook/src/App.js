fb200877afb981760627da9926456c9f98aa8687
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-26 11:26:00 +0530

    useRef Hook Part 1

--- callback-hook/src/App.js
+++ callback-hook/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import React from "react";
 import ParentComponent from "./components/ParentComponent";
 import Counter from "./components/Counter";
+import FocusInput from "./components/FocusInput";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <FocusInput />
+      {/* <Counter /> */}
       {/* <ParentComponent /> */}
     </div>
   );
