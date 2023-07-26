9e73f8ffd28b67a2b01f88dab100592be8954c00
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-26 11:06:28 +0530

    useMemo Hook

--- callback-hook/src/App.js
+++ callback-hook/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import ParentComponent from "./components/ParentComponent";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <ParentComponent />
+      <Counter />
+      {/* <ParentComponent /> */}
     </div>
   );
 }
---
+++ callback-hook/src/components/Counter.js
@@ -0,0 +1,31 @@
+import React, { useMemo, useState } from "react";
+
+function Counter() {
+  const [counterOne, setCounterOne] = useState(0);
+  const [counterTwo, setCounterTwo] = useState(0);
+
+  const incrementOne = () => {
+    setCounterOne(counterOne + 1);
+  };
+  const incrementTwo = () => {
+    setCounterTwo((prev) => prev + 1);
+  };
+
+  const isEven = useMemo(() => {
+    let i = 0;
+    while (i < 2000000000) i++;
+    return counterOne % 2 === 0;
+  }, [counterOne]);
+
+  return (
+    <div>
+      <div>
+        <button onClick={incrementOne}>Count One - {counterOne}</button>
+        <span>{isEven ? "Even" : "Odd"}</span>
+        <button onClick={incrementTwo}>Count Two - {counterTwo}</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
