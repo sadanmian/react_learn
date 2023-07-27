9cfff5e44ef3e906ca120a68455ff108eb293119
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-27 17:56:20 +0530

    React Redux Setup

---
+++ react-redux/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CakeContainer from "./components/CakeContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default App;
