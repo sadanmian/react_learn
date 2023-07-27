f5e5bc457cbff2bc979fe7b39bffe3278f7f2831
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-27 11:12:14 +0530

    Reducers

--- redux/index.js
+++ redux/index.js
@@ -1,8 +1,29 @@
 const BUY_CAKE = "BUY_CAKE";
 
+// Action
 function buyCake() {
   return {
     type: BUY_CAKE,
     info: "First reduc action",
   };
 }
+
+// Reducer
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (currentState = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...currentState,
+        numOfCakes: currentState.numOfCakes - 1,
+      };
+      break;
+
+    default:
+      return currentState;
+      break;
+  }
+};
