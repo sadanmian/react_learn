1efcdc0c7ff726e7546915a5ba7540b8676544c0
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-27 18:13:32 +0530

    Store

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,12 +1,16 @@
-import logo from "./logo.svg";
+import React from "react";
+import { Provider } from "react";
+import store from "./redux/store";
 import "./App.css";
 import CakeContainer from "./components/CakeContainer";
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
 
