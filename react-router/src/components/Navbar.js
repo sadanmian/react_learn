82d8e46b15e9c473620dee4960a2d68558a1760e
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-29 09:51:37 +0530

    Links

---
+++ react-router/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <Link to={"/"}>Home Icon</Link>
+        <Link to={"about"}>About Icon</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
