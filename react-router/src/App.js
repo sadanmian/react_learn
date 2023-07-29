e9a81ba3bf389a4239fe2471ba70089074f8d624
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-29 12:12:26 +0530

    Dynamic Routes

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -7,6 +7,9 @@ import NoMatch from "./components/NoMatch";
 import Products from "./components/Products";
 import FeaturedProducts from "./components/FeaturedProducts";
 import NewProducts from "./components/NewProducts";
+import Users from "./components/Users";
+import UserDetails from "./components/UserDetails";
+import { Admin } from "./components/Admin";
 
 function App() {
   return (
@@ -20,6 +23,10 @@ function App() {
           <Route index element={<FeaturedProducts />} />
           <Route path="featured" element={<FeaturedProducts />} />
           <Route path="new" element={<NewProducts />} />
+        </Route>
+        <Route path="users" element={<Users />}>
+          <Route path=":userId" element={<UserDetails />} />
+          <Route path="admin" element={<Admin />} />
         </Route>
         <Route path="*" element={<NoMatch />} />
       </Routes>
