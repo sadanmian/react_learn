58bd23bbb4a9a19ec407ca54ebdd297791f74572
Sadan Mian <sadanmiyan007@gmail.com>
2023-09-14 19:45:18 +0530

    Store

--- Redux ToolKit/index.js
+++ Redux ToolKit/index.js
@@ -1,5 +1,9 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 // Action
 const CAKE_ORDERED = "CAKE_ORDERED";
+// Action creater
 const orderCake = () => {
   return {
     type: CAKE_ORDERED,
@@ -10,7 +14,6 @@ const orderCake = () => {
 // Initial State
 const initialState = {
   numOfCakes: 10,
-  anotherProperty: 0,
 };
 
 // Reducer
@@ -26,3 +29,20 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+// Redux store
+
+//1 Hold Application state
+const store = createStore(reducer);
+//2 Allows access to state via getState()
+console.log("Initial State", store.getState());
+//4 Registers listeners via subscribe(listener)
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated State", store.getState())
+);
+//3 Allows state to be updated via dispatch(action)
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+//4 Handles unregistering of listeners via the function returned by subscribe(listener)
+unsubscribe();
