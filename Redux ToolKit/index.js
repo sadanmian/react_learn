907dde2d74e6589ce3b46b7fef26ef29b22f5d08
Sadan Mian <sadanmiyan007@gmail.com>
2023-09-14 19:15:19 +0530

    Reducer

--- Redux ToolKit/index.js
+++ Redux ToolKit/index.js
@@ -1,8 +1,28 @@
+// Action
 const CAKE_ORDERED = "CAKE_ORDERED";
-
 const orderCake = () => {
   return {
     type: CAKE_ORDERED,
     quantity: 1,
   };
+};
+
+// Initial State
+const initialState = {
+  numOfCakes: 10,
+  anotherProperty: 0,
+};
+
+// Reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
 };
