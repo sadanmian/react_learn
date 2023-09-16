bcff080e416a7aaed67a5e1d8bb5802827d84715
Sadan Mian <sadanmiyan007@gmail.com>
2023-09-16 11:21:12 +0530

    Async Thunks

--- Redux ToolKit/rtk-demo/app/store.js
+++ Redux ToolKit/rtk-demo/app/store.js
@@ -1,14 +1,16 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 // const reduxLogger = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice");
+const userReducer = require("../features/user/userSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
 
 // const logger = reduxLogger.createLogger();
 
 const store = configureStore({
   reducer: {
-    cake: cakeReducer,
-    icecream: icecreamReducer,
+    // cake: cakeReducer,
+    // icecream: icecreamReducer,
+    user: userReducer,
   },
   // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
